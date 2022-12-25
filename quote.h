4b1ac39d55a00fba9e9b354d174db88ea5a92e64
guzzyman <gbenga.isess@gmail.com>
2022-12-25 23:21:13 +0100

    Simple shell task rewrite

--- quote.h
+++ quote.h
@@ -1,170 +1,44 @@
-#include "quote.h"
+#ifndef _QUOTE_H_
+#define _QUOTE_H_
+
+#include <stdlib.h>
+
+#include "ctype.h"
+#include "string.h"
 
 /**
- * dequote - dequote a string
- * @str: the string to dequote
- * Return: If memory allocation fails, return NULL.
- * Otherwise return a dequoted copy of str.
+ * enum quote_state - a quote state mnemonic
+ * @QUOTE_NONE: In an unquoted sequence of blanks
+ * @QUOTE_WORD: In an unquoted sequence of non-blanks
+ * @QUOTE_DOUBLE: In double quotes
+ * @QUOTE_SINGLE: In single quotes
+ * @QUOTE_ESCAPE: Following a backslash
  */
-
-char *dequote(const char *str)
-
+typedef enum quote_state
 {
+	QUOTE_NONE   = 0x0,
+	QUOTE_WORD   = 0x1,
+	QUOTE_DOUBLE = 0x2,
+	QUOTE_SINGLE = 0x4,
+	QUOTE_ESCAPE = 0x8
+} quote_state_t;
 
-	char *new;
+typedef size_t (*quote_state_fp)(const char *, quote_state_t *);
 
-	size_t len = 0, state_len;
+quote_state_t quote_state(char c);
 
-	quote_state_t state;
+quote_state_fp quote_state_f(quote_state_t s);
 
+size_t quote_state_len(const char *str, quote_state_t state);
+size_t _quote_state_none(const char *str, quote_state_t *state);
+size_t _quote_state_word(const char *str, quote_state_t *state);
+size_t _quote_state_double(const char *str, quote_state_t *state);
+size_t _quote_state_single(const char *str, quote_state_t *state);
+size_t _quote_state_escape(const char *str, quote_state_t *state);
 
+int _isspecial_double(char c);
 
-	if (!str)
+char *dequote(const char *str);
+size_t dequote_len(const char *str);
 
-		return (NULL);
-
-
-
-	new = malloc(sizeof(char) * (dequote_len(str) + 1));
-
-	if (!new)
-
-		return (NULL);
-
-
-
-	while (*str)
-
-	{
-
-		state = quote_state(*str);
-
-		str += (1 && (state & (QUOTE_DOUBLE | QUOTE_SINGLE | QUOTE_ESCAPE)));
-
-		state_len = quote_state_len(str, state);
-
-		if (state & QUOTE_DOUBLE)
-
-		{
-
-			for ( ; state_len; --state_len)
-
-			{
-
-				if (quote_state(*str++) & QUOTE_ESCAPE)
-
-				{
-
-					if (*str == '\n')
-
-					{
-
-						++str, --state_len;
-
-						continue;
-
-					}
-
-					if (_isspecial_double(*str))
-
-						++str, --state_len;
-
-				}
-
-				new[len++] = str[-1];
-
-			}
-
-		}
-
-		_memcpy(new + len, str, state_len);
-
-		len += state_len;
-
-		str += state_len;
-
-		str += (*str && (state & (QUOTE_DOUBLE | QUOTE_SINGLE)));
-
-	}
-
-	new[len] = '\0';
-
-	return (new);
-
-}
-
-
-
-
-
-/**
- * dequote_len - compute the length of a string after dequoting
- * @str: the string to evaluate
- * Return: Return the length of str after dequoting
- */
-
-size_t dequote_len(const char *str)
-
-{
-
-	size_t len = 0, state_len;
-
-	quote_state_t state;
-
-
-
-	while (*str)
-
-	{
-
-		state = quote_state(*str);
-
-		str += (1 && (state & (QUOTE_DOUBLE | QUOTE_SINGLE | QUOTE_ESCAPE)));
-
-		state_len = quote_state_len(str, state);
-
-		if (state & QUOTE_DOUBLE)
-
-		{
-
-			for ( ; state_len; --state_len)
-
-			{
-
-				if (quote_state(*str++) & QUOTE_ESCAPE)
-
-				{
-
-					if (*str == '\n')
-
-					{
-
-						++str, --state_len;
-
-						continue;
-
-					}
-
-					if (_isspecial_double(*str))
-
-						++str, --state_len;
-
-				}
-
-				len++;
-
-			}
-
-		}
-
-		len += state_len;
-
-		str += state_len;
-
-		str += (*str && (state & (QUOTE_DOUBLE | QUOTE_SINGLE)));
-
-	}
-
-	return (len);
-
-}
+#endif /* _QUOTE_H_ */
